9b4ac6f9f67636d328cc6b5c01678f68268517b8
parker-ford <35358924+parker-ford@users.noreply.github.com>
2023-11-13 18:26:59 -0800

    Camera Controller update

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,95 +1,50 @@
-using UnityEngine;
 using System.Collections;
- 
-public class CameraController : MonoBehaviour {
- 
-    /*
-    Writen by Windexglow 11-13-10.  Use it, edit it, steal it I don't care.  
-    Converted to C# 27-02-13 - no credit wanted.
-    Simple flycam I made, since I couldn't find any others made public.  
-    Made simple to use (drag and drop, done) for regular keyboard layout  
-    wasd : basic movement
-    shift : Makes camera accelerate
-    space : Moves camera on X and Z axis only.  So camera doesn't gain any height*/
-     
-    public bool useMouse;
+using System.Collections.Generic;
+using UnityEngine;
 
-    float mainSpeed = 10.0f; //regular speed
-    float shiftAdd = 250.0f; //multiplied by how long shift is held.  Basically running
-    float maxShift = 100.0f; //Maximum speed when holdin gshift
-    float camSens = 0.1f; //How sensitive it with mouse
-    private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
-    private float totalRun= 1.0f;
-    private float rotationSpeed = 100f;
-    void Update () {
-        // if(useMouse){
-        //     lastMouse = Input.mousePosition - lastMouse ;
-        //     lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0 );
-        //     lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x , transform.eulerAngles.y + lastMouse.y, 0);
-        //     transform.eulerAngles = lastMouse;
-        //     lastMouse =  Input.mousePosition;
-        // }
-        if(Input.GetKey(KeyCode.RightArrow)){
-            transform.localRotation *= Quaternion.AngleAxis(rotationSpeed * Time.deltaTime, Vector3.up);
-        }
-        if(Input.GetKey(KeyCode.LeftArrow)){
-            transform.localRotation *= Quaternion.AngleAxis(-rotationSpeed * Time.deltaTime, Vector3.up);
-        }
-         if(Input.GetKey(KeyCode.UpArrow)){
-            transform.localRotation *= Quaternion.AngleAxis(rotationSpeed * Time.deltaTime, Vector3.right);
-        }
-        if(Input.GetKey(KeyCode.DownArrow)){
-            transform.localRotation *= Quaternion.AngleAxis(-rotationSpeed * Time.deltaTime, Vector3.right);
-        }
-        //Mouse  camera angle done.  
-       
-        //Keyboard commands
-        Vector3 p = GetBaseInput();
-        if (p.sqrMagnitude > 0){ // only move while a direction key is pressed
-          if (Input.GetKey (KeyCode.LeftShift)){
-              totalRun += Time.deltaTime;
-              p  = p * totalRun * shiftAdd;
-              p.x = Mathf.Clamp(p.x, -maxShift, maxShift);
-              p.y = Mathf.Clamp(p.y, -maxShift, maxShift);
-              p.z = Mathf.Clamp(p.z, -maxShift, maxShift);
-          } else {
-              totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
-              p = p * mainSpeed;
-          }
-         
-          p = p * Time.deltaTime;
-          Vector3 newPosition = transform.position;
-          if (Input.GetKey(KeyCode.Space)){ //If player wants to move on X and Z axis only
-              transform.Translate(p);
-              newPosition.x = transform.position.x;
-              newPosition.z = transform.position.z;
-              transform.position = newPosition;
-          } else {
-              transform.Translate(p);
-          }
-        }
+public class CameraController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    Vector2 currentMouse;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
     }
-     
-    private Vector3 GetBaseInput() { //returns the basic values, if it's 0 than it's not active.
-        Vector3 p_Velocity = new Vector3();
-        if (Input.GetKey (KeyCode.W)){
-            p_Velocity += new Vector3(0, 0 , 1);
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 lastMouse = currentMouse;
+        currentMouse = Input.mousePosition;
+        Vector2 deltaMouse = lastMouse - currentMouse;
+
+        
+        if(Input.GetMouseButton(1)){
+            Debug.Log(deltaMouse);
+            if(Input.GetKey(KeyCode.W)){
+                transform.position += transform.forward * Time.deltaTime * moveSpeed;
+            }
+            if(Input.GetKey(KeyCode.S)){
+                transform.position -= transform.forward * Time.deltaTime * moveSpeed;
+            }
+            if(Input.GetKey(KeyCode.D)){
+                transform.position += transform.right * Time.deltaTime * moveSpeed;
+            }
+            if(Input.GetKey(KeyCode.A)){
+                transform.position -= transform.right * Time.deltaTime * moveSpeed;
+            }
+            if(Input.GetKey(KeyCode.E)){
+                transform.position += transform.up * Time.deltaTime * moveSpeed;
+            }
+            if(Input.GetKey(KeyCode.Q)){
+                transform.position -= transform.up * Time.deltaTime * moveSpeed;
+            }
+
+            //transform.RotateAround(transform.up, deltaMouse.y);
+            transform.Rotate(Vector3.up, -deltaMouse.x * Time.deltaTime * 50f);
+            transform.Rotate(Vector3.right, deltaMouse.y* Time.deltaTime * 50f);
+
         }
-        if (Input.GetKey (KeyCode.S)){
-            p_Velocity += new Vector3(0, 0, -1);
-        }
-        if (Input.GetKey (KeyCode.A)){
-            p_Velocity += new Vector3(-1, 0, 0);
-        }
-        if (Input.GetKey (KeyCode.D)){
-            p_Velocity += new Vector3(1, 0, 0);
-        }
-        if (Input.GetKey (KeyCode.E)){
-            p_Velocity += new Vector3(0, 1, 0);
-        }
-        if (Input.GetKey (KeyCode.Q)){
-            p_Velocity += new Vector3(0, -1, 0);
-        }
-        return p_Velocity;
     }
 }
